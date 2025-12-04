29481d25d33ca90e4ba8eca8ce5ba4c45da201a9
hyacehila <hyacehila@gmail.com>
2025-12-04 12:32:47 +0800

    Big Change

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,6 +145,7 @@ for (let i = 0; i < filterBtn.length; i++) {
 
 
 
+
 // contact form variables
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
